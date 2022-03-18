0a71729b6bba77b8e1b1a9d6a6d50d35e72ad3bf
Skelektronik <Acedia_the_Skelektronik@gmx.de>
2022-03-18 18:45:27 +0100

    complete exercise, mouse leaves a red trail

--- script.js
+++ script.js
@@ -3,19 +3,34 @@ const button = document.querySelector('button');
 button.addEventListener('click', () => {
     let ask = parseInt(prompt("How Many Gridobjects?"));
     ask > 100 || isNaN(ask) ?  alert('Invalid amount') : createObjects(ask);
-    
+ 
+    const container = document.querySelector('#container');
+
+const blocks = container.querySelectorAll('.block');
+  blocks.forEach((block) => {
+        block.addEventListener('mouseover', () => {
+            // let factor = 255;
+            block.style.backgroundColor = "red";
+            //block.style.backgroundColor = `rgb(${Math.random * factor}, ${Math.random * factor}, ${Math.random * factor})`;
+            // if (factor > 0) {
+            //     return () => {
+            //       factor--; return factor;
+            // } };
+        });
+    });
+
 });
 
 function createObjects(amount) {
     const container = document.querySelector('#container')
     for(let i = 0; i < amount; i++) {
         const block = document.createElement('div');
+        block.classList.add('hold')
         for(let j = 0; j < amount; j++) {
             const BLOCKS = document.createElement('div');
-            BLOCKS.textContent = j;
+            BLOCKS.classList.add('block')
             block.appendChild(BLOCKS);
         };
-        // block.style.cssText = "flex-grow: 1; flex-shrink: 0; flex-basis: auto; padding: 10px"
         container.appendChild(block);
     }
 }
