7e056ec895aeda1963eb5e7af91f03918fe3e32d
Skelektronik <Acedia_the_Skelektronik@gmx.de>
2022-03-31 22:19:57 +0200

    add conformity

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ button.addEventListener('click', () => {
     if(document.querySelector('.container')) {
         const temp = document.querySelector('.container');
         temp.remove();
-
     }
     const body = document.querySelector('body');
     const container = document.createElement('div');
@@ -15,21 +14,20 @@ button.addEventListener('click', () => {
 
 
 
-    let ask = parseInt(prompt("How Many Gridobjects?"));
+    let ask = parseInt(prompt('How Many Gridobjects?'));
     ask > 100 || isNaN(ask) ?  alert('Invalid amount \nEnter: 1 - 100') : createObjects(ask);
  
 const blocks = document.querySelectorAll('.block');
   blocks.forEach((block) => {
         block.addEventListener('mouseover', () => {
-            let color = document.querySelector("select").value;
-            if (color === "colorful") {
+            let color = document.querySelector('select').value;
+            if (color === 'colorful') {
                 color = `rgb(${Math.random() * 255}, 
                 ${Math.random() * 255}, ${Math.random() * 255})`;
-            }
+            };
             block.style.backgroundColor = color;
         });
     });
-
 });
 
 
@@ -38,10 +36,10 @@ function createObjects(amount) {
     const container = document.querySelector('.container')
     for(let i = 0; i < amount; i++) {
         const block = document.createElement('div');
-        block.classList.add('hold')
+        block.classList.add('hold');
         for(let j = 0; j < amount; j++) {
             const BLOCKS = document.createElement('div');
-            BLOCKS.classList.add('block')
+            BLOCKS.classList.add('block');
             block.appendChild(BLOCKS);
         };
         container.appendChild(block);
